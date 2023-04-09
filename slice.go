30267eddc0ce8d6e325bb6e5d2fc7a623aca7317
Shiliang.Tong <tongsl1003@gmail.com>
2023-04-09 22:13:08 +0200

    add slice

---
+++ slice.go
@@ -0,0 +1,13 @@
+package golab
+
+func Contains(alpha []string, str string) bool {
+	// iterate using the for loop
+	for i := 0; i < len(alpha); i++ {
+		// check
+		if alpha[i] == str {
+			// return true
+			return true
+		}
+	}
+	return false
+}
